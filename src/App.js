65630a8d44b86e25c8446393472b240d6933eb82
kittyhawk-sailor <48333506+VishalESD@users.noreply.github.com>
2022-05-06 13:39:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ class App extends Component {
     this.state = {
       monsters: [],
     }
+    console.log('constructor');
   }
 
   componentDidMount(){
+    console.log('componentDidMount');
+
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) => this.setState(
@@ -26,6 +29,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
     return (
       <div className="App">
         {
