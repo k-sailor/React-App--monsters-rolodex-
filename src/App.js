6cb9b52fbc3fcdea1a2bb993e4c8a6cb7641708e
khs56 <vishal.mudi@esd.ny.gov>
2022-07-04 19:37:41 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { Component } from 'react';
 import logo from './logo.svg';
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -48,21 +51,8 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input className='search-box' type='search' placeholder='search monsters'
-          onChange={onSearchChange}
-        />
-        {
-          filteredMonsters.map((monster) => {
-            return (
-              <div key={monster.id}>
-              <h1>
-                {monster.name}
-              </h1>
-            </div>
-            );
-          })
-        }
-        {/* <button>Test</button> */}
+        <SearchBox onChangeHandler={onSearchChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
